03862c349c45209bec7127069d6398e8ea93662b
something1432 <anmol_gill09@live.com>
2023-11-30 13:13:20 -0800

    Test code about masking and working with HSV.

---
+++ Test code/AnmolCursorPixelValue.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2 as cv
+import os
+import sys
+'''
+This code lets you click on a pixel and diplays its values on the image
+'''
+directoryPath = r'C:\Users\anmol\Documents\UBC\MECH 4\MECH 423\Labs\Final Project\Mech423\Images';
+
+#Load pool table & pool balls
+#imgPoolTable = cv.imread(os.path.join(directoryPath,'American-style_pool_table_diagram_(empty).png'))
+imgNormal = cv.imread(os.path.join(directoryPath,'Poolballs.png'))
+img = cv.cvtColor(imgNormal,cv.COLOR_BGR2HSV)
+
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+    if event == cv.EVENT_LBUTTONDBLCLK:
+        pixel_value = img[y, x]
+        pixel_value_str = f"Pixel Value: {pixel_value}"
+        font = cv.FONT_HERSHEY_SIMPLEX
+        cv.putText(img, pixel_value_str, (x, y), font, 1, (255, 255, 255), 2, cv.LINE_AA)
+
+# Create a black image, a window and bind the function to window
+cv.namedWindow('image',cv.WINDOW_KEEPRATIO)
+#cv.resizeWindow('image', 1000, 1000)
+cv.setMouseCallback('image',draw_circle)
+
+while(1):
+    cv.imshow('image',img)
+    if cv.waitKey(20) & 0xFF == 27:
+        break
+cv.destroyAllWindows()
---
+++ Test code/AnmolPoolTableTest.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2 as cv
+import sys
+import os
+from matplotlib import pyplot as plt
+
+"""
+This code places pictures of pool balls on top of a pool table
+"""
+
+directoryPath = r'C:\Users\anmol\Documents\UBC\MECH 4\MECH 423\Labs\Final Project\Mech423\Images';
+
+#Load pool table & pool balls
+imgPoolTable = cv.imread(os.path.join(directoryPath,'American-style_pool_table_diagram_(empty).png'))
+imgPoolBalls = cv.imread(os.path.join(directoryPath,'Poolballs.png'))
+
+#Make sure both images succesfully loaded
+assert imgPoolTable is not None, "file could not be loaded"
+assert imgPoolBalls is not None, "file could not be loaded"
+
+#define region of interest (ROI) at top left corner of pool table size of pool balls img
+rows,cols,channels = imgPoolBalls.shape
+imgPoolBalls = cv.resize(imgPoolBalls, (800,800))
+rows,cols,channels = imgPoolBalls.shape
+roi = imgPoolTable[0:rows,0:cols]
+
+#Create mask of image
+imgPoolBallsBlue = imgPoolBalls[:,:,0]
+imgPoolBallsGreen = imgPoolBalls[:,:,1]
+imgPoolBallsRed = imgPoolBalls[:,:,2]
+imgPoolBallsHSV = cv.cvtColor(imgPoolBalls,cv.COLOR_BGR2HSV)
+# Define the range of green in HSV
+lower_green = np.array([57, 190, 136])
+upper_green = np.array([63, 196, 142])
+mask_inv = cv.inRange(imgPoolBallsHSV, lower_green, upper_green)
+mask = cv.bitwise_not(mask_inv)
+
+# Now black-out the area of logo in ROI
+imgPoolTable_bg = cv.bitwise_and(roi,roi,mask = mask_inv)
+# Take only region of logo from logo image.
+imgPoolBalls_fg = cv.bitwise_and(imgPoolBalls,imgPoolBalls,mask = mask)
+
+#Display an image
+dst = cv.add(imgPoolTable_bg,imgPoolBalls_fg)
+imgPoolTable[0:rows, 0:cols ] = dst
+cv.imshow('res',imgPoolTable)
+cv.waitKey(0)
+cv.destroyAllWindows()
