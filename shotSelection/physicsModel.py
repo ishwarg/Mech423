f4d70b800924afb8c81621aa0f0010fe2531d80d
something1432 <anmol_gill09@live.com>
2023-12-07 17:05:52 -0800

    Physics model implemeted

--- shotSelection/physicsModel.py
+++ shotSelection/physicsModel.py
@@ -4,27 +4,60 @@ from objectRecognition import cueAngle as ca , ballDetection as bd, calibration 
 from PoolTableConstants import *
 
 RADIUS = cc.RADIUS# in mm
-POCKETS = [(0,0),(0,MAX_HEIGHT),(MAX_WIDTH/2,MAX_HEIGHT),(MAX_WIDTH,MAX_HEIGHT),(MAX_WIDTH,0)]
+POCKETS = [(0,0),(0,MAX_HEIGHT),(MAX_WIDTH/2,MAX_HEIGHT),(MAX_WIDTH,MAX_HEIGHT),(MAX_WIDTH,0),(MAX_WIDTH/2,0)]
 
-#takes id of objectBall and cueBall
-#takes list of ball coordinates and list of pocket coordinates
-def idealAngle(objectBallIndex, cueBallIndex, balls, pocketIndex):
-    Pockets = [(0,0)]
+#Function to see if there are ball collisions
+#Input: 
+# List of all ball coords
+# list of balls to filter out (first ball is chosen as start position)
+# Trajectory of ball
+#Output
+# True or false ball colision
+def CheckCircleCollision(balls, ballIndices, traj):
+    
+    line_start = balls[ballIndices[0]]
+    for i,circle_center in enumerate(balls):
+        if i in ballIndices:
+            continue
 
-    objectCoordinates = balls[objectBallIndex]
-    cueBallCoordinates = balls[cueBallIndex]
+        # Vector from the start of the line to the circle center
+        start_to_center = circle_center - line_start
 
-    ox = objectCoordinates[0]
-    oy = objectCoordinates[1]
-    cx = cueBallCoordinates[0]
-    cy = cueBallCoordinates[1]
+        # Calculate the projection of start_to_center onto the line_vector
+        projection = np.dot(start_to_center, traj) / np.dot(traj, traj)
 
-    return None
+        # Calculate the closest point on the line to the circle center
+        closest_point = line_start + projection * traj
 
-def ObjectBallTraj(balls,ballIndex,pocket):
-    traj = Pockets[pocketIndex] - balls[ballIndex]
-    return traj
+        # Calculate the distance between the closest point and the circle center
+        distance = np.linalg.norm(closest_point - circle_center)
+        
+        # Check if the distance is less than or equal to the radius
+        if distance <= RADIUS:
+            return True
+        
+    return False
 
+#Function to see if there are ball collisions
+#Input: 
+# List of all ball coords
+# Ball to generate traj
+# Trajectory of ball
+#Output
+# True or false ball colision   
+def ObjectBallTraj(balls,ballIndex,pocketIndex):
+
+    traj = POCKETS[pocketIndex] - balls[ballIndex]
+    Collision = CheckCircleCollision(balls, ballIndex, traj)
+    return Collision, traj
+
+def CueBallTraj(balls,objectBallIndex,cueBallIndex,objectBallTraj):
+    Line_end = objectBallTraj/-np.linalg.norm(objectBallTraj)*2*RADIUS + balls[objectBallIndex]
+    Line_start = balls[cueBallIndex]
+    traj = Line_start - Line_end
+    Collision = CheckCircleCollision(balls, [cueBallIndex,objectBallIndex], traj)
+
+    return Collision, traj
 
 
 
