3c15bc1d8e1b0d42d328e61f9e3a9bd788e63322
Ishwarjot Grewal <ishwarjotgrewal@hotmail.com>
2023-12-19 12:23:31 -0800

    qd

--- objectRecognition/PoolTableConstants.py
+++ objectRecognition/PoolTableConstants.py
@@ -3,7 +3,7 @@ import numpy as np
 WHITE_THRESHOLD = 10    #Threshold for the whit part of the ball
 PERCENT_WHITE_THRESHOLD = 0.5   #Threshold for what percentage of the ball is white before it is considered a stiped ball
 MINIMUM_RADIUS = 25     #Minimum pool ball radius to expect in the image
-MAXIMUM_RADIUS = 80     #Max pool ball radius to expect in the image
+MAXIMUM_RADIUS = 45     #Max pool ball radius to expect in the image
 MAX_WIDTH = 2000
 MAX_HEIGHT = 1000
 IMGSIZE = [MAX_HEIGHT, MAX_WIDTH]
